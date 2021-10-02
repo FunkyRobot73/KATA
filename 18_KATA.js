52e312d53365c8770143e157b8d42cb3113e5549
carlos <FunkyRobot73>
2021-10-01 23:38:14 -0400

    Got KATA #18 Working

--- 18_KATA.js
+++ 18_KATA.js
@@ -1,18 +1,32 @@
 const squareCode = function(message) {
   // Put your solution here
-  message = message.replace(/ /g, "")
-  let count = (Math.ceil(Math.sqrt(message.length)))
-  array = (message.slice(0,count));
+  let rows = [];
+  let output = [];
+  let columnsNum = message.replace(/ /g,"");
+// remove the space
+let noSpaces = Math.ceil(Math.sqrt(columnsNum.length));
+// calculate the number of noSpaces total
+columnsNum = columnsNum.split("");
+
+for (let x = 0; x < columnsNum.length; x++) {
+  rows.push(columnsNum[x]);
+    if ((x + 1) % noSpaces === 0 && (x + 1) !== columnsNum.length) 
+      rows.push(" ");
+};
+rows = rows.join("");
+
+for (let i = 0; i < noSpaces; i++) {
+  output += "\n" + rows[i];
+    for (let j = 0; j < rows.length; j++) {
   
-  //console.log(array);
-  if(message.length > 0) {
+if (rows[j - i - 1] === " ") {
+  output += rows[j];
+      };
 
-    // array.push(message.slice(0,count))
-    // console.log(message);
-    // console.log(array)
-  }
+    };
 
-  return array
+  };
+return output;
 };
 
 console.log(squareCode("chill out"));
